0a51d13996e43e39e16058f2dfa5d1e9edb7fdeb
And12Mary <62303033+And12Mary@users.noreply.github.com>
2020-10-08 19:52:35 +0300

    Add files via upload

--- camera.js
+++ camera.js
@@ -1,4 +1,6 @@
 var video=document.getElementById("video");
+video.addEventListener("touchstart".capteaza);
+video.addEventListener("mousedown".capteaza);
 //------------
 function on_cam_success(stream)
 {
@@ -20,10 +22,9 @@ function capteaza()
 	var c=document.getElementById("canvas");
 	c.width=video.width;
 	c.height=video.height;
-	//var ctx=c.getContext("2d");
+	var ctx=c.getContext("2d");
 	ctx.drawImage(video,0,0,640,480);
-	video.addEventListener("touchstart".capteaza);
-	video.addEventListener("mousedown".capteaza);
+	
 	
 }
 //-------------------
